06d93532edd20b4f7bce8d4d8cf30e3aa420a09b
root <root@localhost>
2021-10-30 09:04:30 +0200

    Added DIY exercise

--- sample.js
+++ sample.js
@@ -4,29 +4,59 @@
 // the setTimeout function can run a function after a certain amount of time
 let greet = (phrase, name) => alert(`${phrase} ${name}!`); 
 
-setTimeout(greet, 1000, 'hello', 'user');
+//setTimeout(greet, 1000, 'hello', 'user');
 
 // setTimeout will return a timer identifier with which it can be cancelled if necessary
 // After cancellation, the id is preserved
-greetTimerId = setTimeout(greet, 10000, 'hello', 'user');
-clearTimeout(greetTimerId);
+//greetTimerId = setTimeout(greet, 10000, 'hello', 'user');
+//clearTimeout(greetTimerId);
 
-alert(greetTimerId);
+//alert(greetTimerId);
 
 
 // setInterval follows the same syntax and calls a function over and over
-greetIntervalId = setInterval(greet, 2000, 'hello', 'world');
+//greetIntervalId = setInterval(greet, 2000, 'hello', 'world');
 
 // We can then schedule clearInterval to stop the loop after 10 seconds
-setTimeout(() => { clearInterval(greetIntervalId); alert('stopped execution'); }, 10000);
+// setTimeout(() => { clearInterval(greetIntervalId); alert('stopped execution'); }, 10000);
 
 
 // To run a task regularly we can also define nested setTimeout
 
 let num = 0;
 
-let countTimerId = setTimeout(function count () {
-    num += 1;
-    alert(num);
-    countTimerId = setTimeout(count, 2000); 
-}, 2000);
+//let countTimerId = setTimeout(function count () {
+//    num += 1;
+//    alert(num);
+//    countTimerId = setTimeout(count, 2000); 
+//}, 2000);
+
+
+// DIY tasks
+
+let printNumbersInterval = function (from, to){
+    count = from
+    countIntervalId = setInterval(function () {
+        console.log(count);
+        count++;
+        if (count==(to+1)) {
+            clearInterval(countIntervalId);
+        }
+    }, 1000);
+}
+
+let printNumbersNested = function (from, to){
+    count = from
+    let increaseCount = function () {
+        console.log(count);
+        count++;
+    }
+    let countTimerId = setTimeout(function countTimer() {
+        increaseCount();
+        if (count < to+1) {
+            countTimerId = setTimeout(countTimer, 1000);
+        }
+    });
+}
+
+printNumbersInterval(1, 10);
